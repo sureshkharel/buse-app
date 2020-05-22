d3e61afff5612c5c5681bc67fbb898e10d3d614f
kharelsu <kharelsu@deakin.edu.au>
2020-05-22 18:45:03 +1000

    user registration code

---
+++ usermanage/client/src/app.config.js
@@ -0,0 +1,6 @@
+export default {
+    url: 'https://{dev-592092.okta.com}',
+    issuer: 'https://{dev-592092.okta.com}/oauth2/default',
+    redirect_uri: window.location.origin + '/implicit/callback',
+    client_id: '{0oacsjdy9qbgpjcb84x6}'
+  };
